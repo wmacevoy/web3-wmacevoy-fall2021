319feaedddb6e2ccb262fd8f28a65184d87c6c46
Warren MacEvoy <wmaceevoy@gmail.com>
2021-09-08 14:53:20 -0600

    add front end

--- hello/main.js
+++ hello/main.js
@@ -2,12 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let ctr = 0
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/bye', (req, res) => {
   res.send('Bye for now!')
+})
+
+app.get('/add', (req, res) => {
+  ctr += 1;
+  let x = JSON.parse(req.query.x);
+  let y = JSON.parse(req.query.y);
+  let z = Number(x) + Number(y);
+
+  res.json({ 'ctr' : ctr , 'x': x, 'y': y, 'z': z});
 })
 
 app.listen(port, () => {
