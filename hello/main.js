628e9b4e0c2d979b331d5ba68204c2553fc30e4e
Warren MacEvoy <wmaceevoy@gmail.com>
2021-09-15 14:53:45 -0600

    add api

--- hello/main.js
+++ hello/main.js
@@ -6,7 +6,6 @@ let ctr = 0
 
 app.use(express.json());
 app.use((req, res, next) => {
-  // res.setHeader('Content-Security-Policy', "style-src 'unsafe-inline'"); // INSECURE
   res.setHeader('Access-Control-Allow-Origin',"*");
   next();
 });
@@ -24,8 +23,9 @@ app.get('/add', (req, res) => {
   let x = JSON.parse(req.query.x);
   let y = JSON.parse(req.query.y);
   let z = Number(x) + Number(y);
-
-  res.json({ 'ctr' : ctr , 'x': x, 'y': y, 'z': z});
+  let obj = { 'ctr' : ctr , 'x': x, 'y': y, 'z': z};
+  console.log("/add: " + JSON.stringify(obj));
+  res.json(obj);
 })
 
 app.listen(port, () => {
